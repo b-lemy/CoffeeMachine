95290cfca58833870d75f8820e16dd4d19bb3355
b-lemy <lemabrian1234@gmail.com>
2022-12-01 14:53:19 +0300

    moreFiles

---
+++ ALX/Assignment 3/Test1.py
@@ -0,0 +1,5 @@
+import string
+
+print(string.ascii_uppercase)
+
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
---
+++ OOP/SYS.py
@@ -0,0 +1,13 @@
+import math
+from datetime import date
+
+print(math.cos(math.pi / 4))
+
+print(math.log(1024, 2))
+
+# about  dates
+
+now = date.today()
+print(now)
+
+n = 2
