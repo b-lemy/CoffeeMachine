283d4bc3472af92ba144e84aa8debe48094ac34c
b-lemy <lemabrian1234@gmail.com>
2022-12-01 17:30:16 +0300

    moreFiles

--- OOP/Fibonacci.py
+++ OOP/Fibonacci.py
@@ -4,9 +4,16 @@ def fib2(n):  # return Fibonacci series up to n
     a, b = 0, 1
     while a < n:
         result.append(a)
+        # They give me different output do you know what's the reason
+        # this
         a, b = b, a + b
+
+        # or this
+
+        a = b
+        b = a + b
+
     return result
 
 
 print(fib2(14))
-
