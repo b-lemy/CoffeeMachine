9549b397731ef7a505c7da0a25fc6fc6f17f2bb5
b-lemy <lemabrian1234@gmail.com>
2022-12-10 13:55:13 +0300

    alx

--- ALX/Assignment 4/RomanToInt.py
+++ ALX/Assignment 4/RomanToInt.py
@@ -51,14 +51,14 @@ def roman_to_int(roman_string):
 roman_number = "XI"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-# roman_number = "VII"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-#
-# roman_number = "IX"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-#
-# roman_number = "LXXXVII"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-#
-# roman_number = "DCCVII"
-# print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
