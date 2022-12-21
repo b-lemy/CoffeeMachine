63686d379cb49cf40f5558342c3cf36b9c573e75
b-lema <75371492+b-lemy@users.noreply.github.com>
2022-12-21 22:25:47 +0300

    dataframes

---
+++ TriphCourse/Day25/DataFrames.py
@@ -0,0 +1,13 @@
+import pandas
+
+data = pandas.read_csv('2018-Central-Park-Squirrel-Census-Squirrel-Data.csv')
+
+print(data[data["Primary Fur Color"] == 'Black'])
+print(len(data[data["Primary Fur Color"] == 'Gray']))
+
+data_dict = {
+    'Fur_Color': ['1', '2', '3'],
+    'Count': ['Red', 'Orange', 'Black']
+}
+df = pandas.DataFrame(data_dict)
+df.to_csv('Trial.csv')
