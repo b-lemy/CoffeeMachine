ad8822b5837bdb1f82c7972c326457d020a9f749
b-lemy <lemabrian1234@gmail.com>
2022-12-21 19:51:35 +0300

    pandas

--- TriphCourse/Day25/RealPandas.py
+++ TriphCourse/Day25/RealPandas.py
@@ -3,4 +3,23 @@
 import pandas
 
 data = pandas.read_csv('weather-data.csv')
+# Get data in Columns
 print(data['temp'])
+# or
+print(data.temp)
+
+data_dict = data.to_dict()
+print(data_dict)
+
+data_list = data['temp'].to_list()
+print(len(data_list))
+
+avg = sum(data_list) / len(data_list)
+print(avg)
+
+# or
+print(data['temp'].mean())
+print(data['temp'].max())
+
+# GET DATA IN ROWS
+print(data[data.day == 'Monday'])
